33e49597129db2dd761e705b9aed174a5db3e151
waqiju <waqiju@gmail.com>
2021-09-21 22:59:10 +0800

    Add: jenkins_job.py

---
+++ jenkins_job.py
@@ -0,0 +1,25 @@
+''' 定时的更新任务，由维护者运行，普通用户请忽视。
+'''
+import os
+import time
+from updater import updater
+
+
+def run():
+    print('== update cn json ==')
+    updater.update_cn_json()
+    print('== push github ==')
+    exit_code = os.system('git commit -am "Sync by robot"')
+    assert(exit_code == 0)
+    exit_code = os.system('git push origin')
+    assert(exit_code == 0)
+    print('== push gitee ==')
+    os.system('git push cn_mirror')
+    assert(exit_code == 0)
+
+    print('job success')
+    time.sleep(5)
+
+
+if __name__ == '__main__':
+    run()
